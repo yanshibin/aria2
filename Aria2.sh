2d04cdc04c6b5bb4b9e0a495dfbd47ab5867caf2
Kristina <8521118@gmail.com>
2018-01-24 21:10:33 +0800

    Update Aria2.sh

--- Aria2.sh
+++ Aria2.sh
@@ -72,7 +72,7 @@ bt-save-metadata=true
 enable-dht=true
 enable-peer-exchange=true
 seed-time=0
-
+bt-tracker=udp://tracker.coppersurfer.tk:6969/announce,udp://tracker.leechers-paradise.org:6969/announce,udp://9.rarbg.to:2710/announce,udp://p4p.arenabg.com:1337/announce,http://p4p.arenabg.com:1337/announce,udp://tracker.internetwarriors.net:1337/announce,http://tracker.internetwarriors.net:1337/,udp://tracker.skyts.net:6969/announce,udp://tracker.safe.moe:6969/announce,udp://tracker.piratepublic.com:1337/announce,udp://tracker.opentrackr.org:1337/announce,http://tracker.opentrackr.org:1337/announce,udp://wambo.club:1337/announce,udp://trackerxyz.tk:1337/announce,udp://tracker4.itzmx.com:2710/announce,udp://tracker2.christianbro.pw:6969/announce,udp://tracker1.wasabii.com.tw:6969/announce,udp://tracker.zer0day.to:1337/announce,udp://public.popcorn-tracker.org:6969/announce,udp://peerfect.org:6969/announce
 EOF
 
 cat >/etc/aria2/aria2<<EOF
